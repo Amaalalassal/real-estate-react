504170f7a5ea57703a1afaf3b6a87360ae29deeb
Amaalalassal <amaalalassall@gmail.com>
2025-04-11 04:10:36 +0200

    ALL

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ImagesSlider from './components/common/ImagesSlider.jsx'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ComparePage from './components/pages/ComparePage';
+import PropertiesGridPage from './components/pages/PropertiesGridPage';
+import { FavoriteProvider } from './components/common/FavoriteContext'; 
+
+const theme = createTheme({
+  components: {
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          color: '#1cb3ff', 
+        },
+      },
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <ImagesSlider/>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <FavoriteProvider> 
+        <Router>
+          <Routes>
+            <Route path="/" element={<PropertiesGridPage />} />
+            <Route path="/compared" element={<ComparePage />} />
+          </Routes>
+        </Router>
+      </FavoriteProvider>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "tailwindcss";
 import App from './App.jsx'
+import CompareContext from './components/common/CompareContext.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <CompareContext>
     <App />
+    </CompareContext>
+
   </StrictMode>,
 )
